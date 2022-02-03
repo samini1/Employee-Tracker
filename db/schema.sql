cacb686cbce05e4392590d45489b40bba7f6b131
samini1 <92944248+samini1@users.noreply.github.com>
2022-02-03 10:43:28 -0600

    added expected foreign key references between tables

--- db/schema.sql
+++ db/schema.sql
@@ -11,13 +11,13 @@ CREATE TABLE roles (
     id INTEGER AUTO_INCREMENT PRIMARY KEY
     title VARCHAR(30)
     salary DECIMAL
-    department_id INTEGER   
+    FOREIGN KEY (department_id) REFERENCES department(id)   
 );
 
 CREATE TABLE employees (
     id INTEGER PRIMARY KEY
     first_name VARCHAR(30)
     last_name VARCHAR(30)
-    role_id INTEGER
-    manager_id INTEGER 
+    FOREIGN KEY (role_id) REFERENCES roles(id)
+    FOREIGN KEY (manager_id) REFERENCES employees(id) 
 );
