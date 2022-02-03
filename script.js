4973907a4b42141342cbdf17de6cad4666a814a9
samini1 <92944248+samini1@users.noreply.github.com>
2022-02-03 10:28:16 -0600

    outlining potential functions needed for start prompt choices

--- script.js
+++ script.js
@@ -1,3 +1,44 @@
 const inquirer = require('inquirer')
 const mysql = require('mysql2')
 const consoleTable = require('console.table');
+
+//prompt when starting app
+function startPrompt() {
+    inquirer.prompt([
+        {
+            type: 'list',
+            message: 'Choose your task',
+            name: 'start',
+            choices: [
+                'View all departments',
+                'View all roles',
+                'View all employees',
+                'Add a department',
+                'Add a role',
+                'Add an employee',
+                'Update an employee role'
+            ]
+        }
+    ])    
+}
+
+//View all departments from start prompt
+function viewDepartments() {};
+
+//view all roles
+function viewRoles() {};
+
+//view all employees
+function viewEmployees() {};
+
+//Add a deparment
+function addDepartment() {};
+
+//Add a role
+function addRole() {};
+
+//Add an employee
+function addEmployee() {};
+
+//Update an employee role
+function updateEmployee() {};
